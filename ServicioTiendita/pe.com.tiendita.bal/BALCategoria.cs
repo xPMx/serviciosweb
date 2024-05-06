1803c85d20820479cb968e222aa100d340fe761a
PM <pieromolfino96@gmail.com>
2024-05-06 15:18:39 -0500

    Subida Inicial
    Se subieron los archivos del proyecto

---
+++ ServicioTiendita/pe.com.tiendita.bal/BALCategoria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pe.com.tiendita.bo;
+using pe.com.tiendita.dal;
+
+
+namespace pe.com.tiendita.bal
+{
+    public class BALCategoria
+    {
+        DALCategoria dalcat = new DALCategoria();
+
+        public List<BOCategoria> MostrarCategoria()
+        {
+            return dalcat.MostrarCategoria();
+        }
+
+        public List<BOCategoria> MostrarCategoriaTodo()
+        {
+            return dalcat.MostrarCategoriaTodo();
+        }
+
+        public bool RegistrarCategoria(BOCategoria bc)
+        {
+            return dalcat.RegistrarCategoria(bc);
+        }
+
+        public bool ActualizarCategoria(BOCategoria bc)
+        {
+            return dalcat.ActualizarCategoria(bc);
+        }
+
+        public bool EliminarCategoria(BOCategoria bc)
+        {
+            return dalcat.EliminarCategoria(bc);
+        }
+    }
+}
---
+++ ServicioTiendita/pe.com.tiendita.ui/frmCategoria.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmCategoria.aspx.cs" Inherits="pe.com.tiendita.ui.frmCategoria" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Mantenimiento de Categoria</h1>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
